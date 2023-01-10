0c2151274b45f2fc8fc71326e3c7037c116a6631
min49590 <min49590@naver.com>
2023-01-10 17:46:43 +0900

    commit 3

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Caver from 'caver-js';
 import * as buffer from "buffer";
 window.Buffer = buffer.Buffer;
 
-const COUNT_CONTRACT_ADDRESS = '0x6C2352A0d0053140c2bf5Eb3dDF017c6860C6E31';
+const COUNT_CONTRACT_ADDRESS = '0x52b415CC3D951a3B707911B542a5F63bF2070aF6';
 const ACCESS_KEY_ID = 'KASKVQPIZU3LJ7M2CE8DS9HU';
 const SECRET_ACCESS_KEY = 'F06s3XjM7GBsq9kJBAF0vcKSbkno4c2zoaRnIq1A';
 const CHAIN_ID = '1001';
 const COUNT_ABI = '[ { "inputs": [], "name": "retrieve", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "num", "type": "uint256" } ], "name": "store", "outputs": [], "stateMutability": "nonpayable", "type": "function" } ]';
 // eslint-disable-next-line
-const author_value = Buffer.from(ACCESS_KEY_ID + ":" + SECRET_ACCESS_KEY)
+const author_value = Buffer.from(ACCESS_KEY_ID + ":" + SECRET_ACCESS_KEY).toString("base64");
 const option = {
  headers: [
   {
@@ -28,7 +28,7 @@ const option = {
 const caver = new Caver(new Caver.providers.HttpProvider("https://node-api.klaytnapi.com/v1/klaytn", option));
 const CountContract = new caver.contract(JSON.parse(COUNT_ABI), COUNT_CONTRACT_ADDRESS)
 const readCount = async() => {
-  const _count = await CountContract.methods.count().call();
+  const _count = await CountContract.methods.retrieve().call();
   console.log(_count);
 };
 
